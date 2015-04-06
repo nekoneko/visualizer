ecaa543addd64205ee18db4bcb5eeb6a40a265bd
nekonkeo <kk357258@gmail.com>
2015-04-07 03:42:12 +0800

    output 'average.txt' in log3.py

--- log3.py
+++ log3.py
@@ -7,14 +7,16 @@
 # Warning : Too many contents may freeze Grasp
 
 log = open('log', 'r')
-fileout = open('context_switch.txt','wb')
+fout_cs = open('context_switch.txt','wb')
+fout_av = open('average.txt','wb')
 lines = log.readlines()
 
-width = 13
+width = 15
 count = 1
 interval = 0.0
+average=0.0
 string = "{:>{width}}{:>{width}}{:>{width}}\n".format('#id','6_digit','10_digit',width=width)
-fileout.write(bytes(string,'UTF-8'))
+fout_cs.write(bytes(string,'UTF-8'))
 
 for line in lines :
 	line = line.strip()
@@ -26,9 +28,18 @@ for line in lines :
 		out_time = (float(tick) + (float(tick_reload) - float(out_minitick)) / float(tick_reload)) / 100
 		in_time  = (float(tick) + (float(tick_reload) - float(in_minitick))  / float(tick_reload)) / 100
 		interval = in_time - out_time
-		string = "{:{width}}{:{width}.6f}{:{width}.10f}\n".format(count,interval,interval,width=width)
-		fileout.write(bytes(string, 'UTF-8'))
+
+		string = "{:>{width}}{:>{width}.6f}{:>{width}.10f}\n".format(count,interval,interval,width=width)
+		fout_cs.write(bytes(string, 'UTF-8'))
+
 		count+=1
+		average+=interval;
+
+average/=count;
+fout_av.write(bytes("{:>{width}}{:>{width}}\n".format('#id','average',width=width),'UTF-8'))
+for i in range(1,count) : 
+	fout_av.write(bytes("{:>{width}}{:>{width}.10f}\n".format(i,average,width=width),'UTF-8'))
 
 log.close()
-fileout.close()
+fout_cs.close()
+fout_av.close()
